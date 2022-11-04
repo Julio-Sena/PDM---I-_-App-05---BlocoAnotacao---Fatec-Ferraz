89e4c1ff48cda62858b38fb058bce9ffe74cf001
Julionilson Sena <julionilson.sena@fatec.sp.gov.br>
2022-11-04 13:02:30 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/bloco_anotacao_kotlin/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.bloco_anotacao_kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bloco_anotacao_kotlin.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
